2e6b5632dd0e7a11d51a4673b5ea9bfb9aa59e1a
Anayah Iqbal <21AIqbal@heckgrammar.co.uk>
2025-03-16 13:52:08 +0000

    j

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -4,11 +4,37 @@
     {
         static void Main(string[] args)
         {
-            // The algorithm in the Readme contains an error
             // Rewrite the algorithm in C# bellow fixing the error
             // Show the test outputs for the test table in the Readme
             // Put your evidence where it says Do the coding task in C#
 
+            bool validChoice = false;
+            int difference = -1;
+            int startyear = 9;
+            int endyear = 2;
+            do
+            {
+                Console.WriteLine("Enter a start year.");
+                startyear = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter an end year.");
+                endyear = Convert.ToInt32(Console.ReadLine());
+
+                if (startyear >= endyear)
+                {
+                    Console.WriteLine("Start year must be before end year.");
+                }
+                if (startyear > 2000)
+                {
+                    Console.WriteLine("Start year must be beofre 2000.");
+                }
+                else 
+                {
+                    validChoice = true;
+                }
+            } while (validChoice == false);
+
+            difference = endyear - startyear;
+            Console.WriteLine("difference: " + difference);
 
         }
     }
